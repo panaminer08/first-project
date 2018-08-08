32197f850ef03d5b91977cee5ed6a9525f06a5a5
panaminer08 <adamsrey@outlook.com>
2018-08-08 19:08:41 -0400

    first project

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById("mybtn").onclick = function (event) {
+alert("Leave me alone DUMMY")
+}
+
+document.getElementById("scndbtn").onclick = function (event) {
+ alert("GET YOUR STANK A*** OUT OF HERE")
+}
+
+
